7962ae8e8dfc5a752d348bb724e0882771267bd1
Tavo2310 <jrom912123@gmail.com>
2022-09-03 20:52:05 -0500

    Pag_Aviones+Rutas

---
+++ ProyectoCiclo3.App.Frontend/Pages/Rutas/List.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Dominio;
+
+ 
+namespace ProyectoCiclo3.App.Frontend.Pages
+{
+    public class ListRutasModel : PageModel
+    {
+    
+ 
+    public void OnGet()
+     {
+
+    } 
+       }
+
+}
