47fea98719e56b570a1e90b313e7e935a95b6072
Tavo2310 <jrom912123@gmail.com>
2022-09-08 05:54:15 -0500

    Repo+Ruta

---
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioRutas.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using ProyectoCiclo3.App.Dominio;
+using System.Linq;
+using System;
+ 
+namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
+{
+    public class RepositorioRutas
+    {
+        List<Rutas> rutas;
+ 
+    public RepositorioRutas()
+        {
+            rutas= new List<Rutas>()
+            {
+                new Rutas{id=1,origen="Manozalez",destino= "Airbus ",tiempo_estimado= "220"},
+                new Rutas{id=1,origen="A321",modelo= "Airbus ",tiempo_estimado= "220"},
+                new Rutas{id=1,origen="A330",modelo= "Airbus ",tiempo_estimado= "293"},
+                new Rutas{id=1,origen="A340",modelo= "Airbus ",tiempo_estimado= "419"},
+                new Rutas{id=1,origen="B737",modelo= "Boeing ",tiempo_estimado= "215"},      
+                new Rutas{id=1,origen="B777",modelo= "Boeing ",tiempo_estimado= "550"},       
+                                
+                 };
+        }
+ 
+        public IEnumerable<Rutas> GetAll()
+        {
+            return rutas;
+        }
+ 
+        public Rutas GetWithId(int id){
+            return rutas.SingleOrDefault(a => a.id == id);
+        }
+    }
+}
