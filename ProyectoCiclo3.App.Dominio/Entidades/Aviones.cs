f276e48f12f977e958080f92e0ddee884c26e5f0
Tavo2310 <jrom912123@gmail.com>
2022-09-08 22:20:48 -0500

    Agrego_Entidad_Aviones

--- ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -10,3 +10,15 @@ namespace ProyectoCiclo3.App.Dominio{
     }
     
  }
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aviones{
+        public int id { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public int numero_asientos  { get; set; }
+        public int numero_toilets  { get; set; }
+        public int capacidad_max_peso  { get; set; }
+    }
+    
+ }
