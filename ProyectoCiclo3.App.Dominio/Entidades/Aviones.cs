e95db581c848e85b007a65d31ed3c174302f79e3
Tavo2310 <jrom912123@gmail.com>
2022-09-08 23:04:04 -0500

    Agreglo_de errores_XD

--- ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -10,15 +10,3 @@ namespace ProyectoCiclo3.App.Dominio{
     }
     
  }
-using System;
-namespace ProyectoCiclo3.App.Dominio{
-    public class Aviones{
-        public int id { get; set; }
-        public string marca { get; set; }
-        public string modelo { get; set; }
-        public int numero_asientos  { get; set; }
-        public int numero_toilets  { get; set; }
-        public int capacidad_max_peso  { get; set; }
-    }
-    
- }
