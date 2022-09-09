4619bc53250ddedda7279c518e335bd5ff350668
Tavo2310 <jrom912123@gmail.com>
2022-09-08 20:29:28 -0500

    Corg Errores 1

--- ProyectoCiclo3.App.Dominio/Entidades/Rutas.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Rutas.cs
@@ -2,8 +2,8 @@ using System;
 namespace ProyectoCiclo3.App.Dominio{
     public class Rutas{
         public int id { get; set; }
-        public origen { get; set; }
-        public destino { get; set; }
+        public string origen { get; set; }
+        public string destino { get; set; }
         public int tiempo_estimado { get; set; }
     } 
 }
