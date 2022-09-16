6bb5ff65f32c6961da90165dd2bedeae29c43160
Tavo2310 <jrom912123@gmail.com>
2022-09-15 22:10:27 -0500

    Base_datos_rutas

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioRutas.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioRutas.cs
@@ -2,34 +2,59 @@ using System.Collections.Generic;
 using ProyectoCiclo3.App.Dominio;
 using System.Linq;
 using System;
- 
+using Microsoft.EntityFrameworkCore;
+
 namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
 {
     public class RepositorioRutas
     {
-        List<Rutas> rutas;
- 
-    public RepositorioRutas()
-        {
-            rutas= new List<Rutas>()
-            {
-                new Rutas{id=1,origen="Manizalez",destino= "Tolima ",tiempo_estimado= 220},
-                new Rutas{id=2,origen="New York",destino= "Soacha ",tiempo_estimado= 320},
-                new Rutas{id=3,origen="Pereira",destino= "Galapa ",tiempo_estimado= 293},
-                new Rutas{id=4,origen="Hula",destino= "Valle ",tiempo_estimado= 419},
-                new Rutas{id=5,origen="Abidabi",destino= "Catar ",tiempo_estimado= 215},      
-                new Rutas{id=6,origen="Buens Aires",destino= "Luqe ",tiempo_estimado= 550}     
-                                
-                 };
-        }
- 
+        private readonly AppContext _appContext = new AppContext();
+
         public IEnumerable<Rutas> GetAll()
         {
-            return rutas;
+            return _appContext.Rutas.Include(u => u.origen)
+                                    .Include(u => u.destino);
         }
- 
-        public Rutas GetWithId(int id){
-            return rutas.SingleOrDefault(a => a.id == id);
+
+        public Rutas GetWithId(int id)
+        {
+            return _appContext.Rutas.Find(id);
         }
+
+        public Rutas Create(int origen, int destino, int tiempo_estimado)
+        {
+            var newRuta = new Rutas();
+            newRuta.destino = _appContext.Aeropuertos.Find(destino);
+            newRuta.origen = _appContext.Aeropuertos.Find(origen);
+            newRuta.tiempo_estimado = tiempo_estimado;
+
+            var addRuta = _appContext.Rutas.Add(newRuta);
+            _appContext.SaveChanges();
+            return addRuta.Entity;
+        }
+
+        public void Delete(int id)
+        {
+            var ruta = _appContext.Rutas.Find(id);
+            if (ruta != null){        
+                _appContext.Rutas.Remove(ruta);
+                _appContext.SaveChanges();
+            }
+        }
+
+        public Rutas Update(Rutas newRuta)
+        {
+            var ruta = _appContext.Rutas.Find(newRuta.id);
+            if (ruta != null)
+            {
+                ruta.origen = newRuta.origen;
+                ruta.destino = newRuta.destino;
+                ruta.tiempo_estimado = newRuta.tiempo_estimado;
+                //Guardar en base de datos
+                _appContext.SaveChanges();
+            }
+            return ruta;
+        }
+
     }
 }
