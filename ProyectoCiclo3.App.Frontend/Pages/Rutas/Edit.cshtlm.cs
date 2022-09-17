ab8eb6a45fbe6b5642661161e66c8e58a57a517c
Tavo2310 <jrom912123@gmail.com>
2022-09-17 09:18:49 -0500

    Rtas_Update

--- ProyectoCiclo3.App.Frontend/Pages/Rutas/Edit.cshtlm.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Rutas/Edit.cshtlm.cs
@@ -12,32 +12,34 @@ namespace ProyectoCiclo3.App.Frontend.Pages
     public class EditRutaModel : PageModel
     {
         private readonly RepositorioRutas repositorioRutas;
+        private readonly RepositorioAeropuertos repositorioAeropuertos;
+        public IEnumerable<Aeropuertos> Aeropuertos {get;set;}
+
         [BindProperty]
         public Rutas Ruta {get;set;}
  
-        public EditRutaModel(RepositorioRutas repositorioRutas)
+        public EditRutaModel(RepositorioRutas repositorioRutas, RepositorioAeropuertos repositorioAeropuertos)
        {
             this.repositorioRutas=repositorioRutas;
+            this.repositorioAeropuertos=repositorioAeropuertos;
        }
- 
+
         public IActionResult OnGet(int rutaId)
         {
-            Ruta=repositorioRutas.GetWithId(rutaId);
+            Aeropuertos = repositorioAeropuertos.GetAll();
+            Ruta = repositorioRutas.GetWithId(rutaId);
             return Page();
+
         }
 
-        public IActionResult OnPost()
+        public IActionResult OnPost(int id, int origen, int destino, int tiempo_estimado)
         {
             if(!ModelState.IsValid)
             {
                 return Page();
-            }
-            if(Ruta.id>0)
-            {
-            //Avion = repositorioRutas.Update(Avion);
-            }
+            }            
+            repositorioRutas.Update(id, origen, destino, tiempo_estimado);
             return RedirectToPage("./List");
         }
-
     }
 }
