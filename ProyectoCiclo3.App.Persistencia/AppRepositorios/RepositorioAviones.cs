4a7bd816be29db88d860e4c909a67c5991795c8a
AldyRhenals <aldy.rj2@hotmail.com>
2022-09-19 10:53:15 -0500

    Base de datos Repositorio aviones

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioAviones.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioAviones.cs
@@ -8,48 +8,47 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
     public class RepositorioAviones
     {
         private readonly AppContext _appContext = new AppContext();
- 
+        
         public IEnumerable<Aviones> GetAll()
         {
             return _appContext.Aviones;
         }
- 
-        public Aviones GetWithId(int id){
+        public Aviones GetWithId(int id)
+        {
             return _appContext.Aviones.Find(id);
         }
-
-        public Aviones Update(Aviones newAvion){
-            var avion = _appContext.Aviones.Find(newAvion.id);
-            if(avion != null){
-                avion.marca = newAvion.marca;
-                avion.modelo = newAvion.modelo;
-                avion.numero_asientos = newAvion.numero_asientos;
-                avion.numero_toilets = newAvion.numero_toilets;
-                avion.capacidad_max_peso = newAvion.capacidad_max_peso;
-                //Guardar en base de datos
-                 _appContext.SaveChanges();
-            }
-            return avion;
-        }
-
         public Aviones Create(Aviones newAvion)
         {
             var addAvion = _appContext.Aviones.Add(newAvion);
-            //Guardar en base de datos
+            // Guardar en base de datos.
             _appContext.SaveChanges();
             return addAvion.Entity;
         }
-
+        public Aviones Update(Aviones newAvion)
+        {
+            var avion = _appContext.Aviones.Find(newAvion.id);
+            if(avion != null)
+            {
+                avion.marca                 = newAvion.marca;
+                avion.modelo                = newAvion.modelo;
+                avion.numero_asientos       = newAvion.numero_asientos;
+                avion.numero_toilets        = newAvion.numero_toilets;
+                avion.capacidad_max_peso    = newAvion.capacidad_max_peso;
+                // Guardar en base de datos.
+                _appContext.SaveChanges();
+            }
+            return avion;
+        }
         public Aviones Delete(int id)
         {
             var avion = _appContext.Aviones.Find(id);
-            if (avion != null){
+            if (avion != null)
+            {
                 _appContext.Aviones.Remove(avion);
-                //Guardar en base de datos
+                // Guardar en base de datos.
                 _appContext.SaveChanges();
             }
             return null;
         }
-
     }
 }
